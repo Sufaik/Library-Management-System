541ca786b181ebaa1ab2b48ae769712bdcd44a2c
Sufaik <sufaikmhamood@gmail.com>
2024-05-27 11:23:14 +0530

    My 2 commit

---
+++ src/Book.java
@@ -0,0 +1,6 @@
+public class Book {
+    public static void main (String[] args){
+        System.out.println("Book_Name");
+        System.out.println("Book_ID");
+        System.out.println("Book_Author");
+}
---
+++ src/LibrarySystem.java
@@ -0,0 +1,7 @@
+public class LibrarySystem {
+    public static void main (String[] args){
+        System.out.println("Library_ID");
+        System.out.println("Library_Name");
+        System.out.println("Library_Admin");
+    }
+}
